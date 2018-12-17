f43ad6e9a6c3bf0e2641d92df0e249b06ef7d451
lytico <lytico@users.sourceforge.net>
2018-12-17 19:37:40 +0100

    XwtContextFormatter: adjust Write(double value) to accept 0

--- Poupou.SvgPathConverter/XwtContextFormatter.cs
+++ Poupou.SvgPathConverter/XwtContextFormatter.cs
@@ -23,15 +23,15 @@ namespace Poupou.SvgPathConverter
 		public XwtContextFormatter (TextWriter textWriter)
 		{
 			writer = textWriter;
-		    
+
 		}
-		
+
 		public virtual void Prologue (string name)
 		{
 			writer.WriteLine ("\tpublic virtual void {0} (Context c)", name);
 			writer.WriteLine ("\t{");
 		}
-	
+
 		public virtual void Epilogue ()
 		{
 			//writer.WriteLine ("\t\tc.FillPreserve ();");
@@ -39,39 +39,40 @@ namespace Poupou.SvgPathConverter
 			writer.WriteLine ("\t}");
 			writer.WriteLine ();
 		}
-	
-        public string Write(double value) {
-            return value.ToString("#.#####",CultureInfo.InvariantCulture);
-        }
+
+		public string Write (double value)
+		{
+			return value.ToString ("0.#####", CultureInfo.InvariantCulture);
+		}
 		public void MoveTo (Point pt)
 		{
-            writer.WriteLine("\t\tc.MoveTo ({0}d, {1}d);", Write(pt.X), Write(pt.Y));
+			writer.WriteLine ("\t\tc.MoveTo ({0}d, {1}d);", Write (pt.X), Write (pt.Y));
 		}
-	
+
 		public void LineTo (Point pt)
 		{
-			writer.WriteLine ("\t\tc.LineTo ({0}d, {1}d);", Write(pt.X), Write(pt.Y));
+			writer.WriteLine ("\t\tc.LineTo ({0}d, {1}d);", Write (pt.X), Write (pt.Y));
 		}
-	
+
 		public void ClosePath ()
 		{
 			writer.WriteLine ("\t\tc.ClosePath ();");
 		}
-	
+
 		public void QuadCurveTo (Point controlPoint, Point endPoint)
 		{
 			writer.WriteLine ("\t\tc.CurveTo ({0}d, {1}d, {2}d, {3}d, {4}d, {5}d);",
-                Write(controlPoint.X), Write(controlPoint.Y), 
-                Write(controlPoint.X), Write(controlPoint.Y), 
-                Write(endPoint.X), Write(endPoint.Y));
+		Write (controlPoint.X), Write (controlPoint.Y),
+		Write (controlPoint.X), Write (controlPoint.Y),
+		Write (endPoint.X), Write (endPoint.Y));
 		}
 
 		public void CurveTo (Point endPoint, Point controlPoint1, Point controlPoint2)
 		{
-			writer.WriteLine ("\t\tc.CurveTo ({0}d, {1}d, {2}d, {3}d, {4}d, {5}d);", 
-				Write(endPoint.X), Write(endPoint.Y), 
-                Write(controlPoint1.X), Write(controlPoint1.Y), 
-                Write(controlPoint2.X), Write(controlPoint2.Y));
+			writer.WriteLine ("\t\tc.CurveTo ({0}d, {1}d, {2}d, {3}d, {4}d, {5}d);",
+				Write (endPoint.X), Write (endPoint.Y),
+		Write (controlPoint1.X), Write (controlPoint1.Y),
+		Write (controlPoint2.X), Write (controlPoint2.Y));
 		}
 
 		public void ArcTo (Point size, Number angle, bool isLarge, bool sweep, Point endPoint, Point startPoint)
